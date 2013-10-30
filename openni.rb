043b9c03c6e4010fac4ef30d3b934702deded8ec
Gleb Gusev <gleb@artec-group.com>
2013-10-30 20:05:42 +0100

    Modified to use my OpenNi version instead of the standard one

--- openni.rb
+++ openni.rb
@@ -2,16 +2,18 @@ require 'formula'
 
 class Openni < Formula
   homepage 'http://www.openni.org/'
-  url 'https://github.com/OpenNI/OpenNI/archive/Stable-1.5.2.23.tar.gz'
-  version 'stable-1.5.2.23'
-  sha1 '1127cd9d82062b1a1ef68d25e39b4b65ac399c79'
+  # url 'https://github.com/OpenNI/OpenNI/archive/Stable-1.5.2.23.tar.gz'
+  # version 'stable-1.5.2.23'
+  # sha1 '1127cd9d82062b1a1ef68d25e39b4b65ac399c79'
+  head 'https://github.com/gag/OpenNI'
 
-  head 'https://github.com/OpenNI/OpenNI.git'
+  # head 'https://github.com/OpenNI/OpenNI.git'
 
   devel do
-    url 'https://github.com/OpenNI/OpenNI/archive/Unstable-1.5.4.0.tar.gz'
-    version 'unstable-1.5.4.0'
-    sha1 '69eb24999b70c46e0befd5eff4a9c9a70d218f41'
+    head 'https://github.com/gag/OpenNI', :branch => 'unstable'
+    # url 'https://github.com/OpenNI/OpenNI/archive/Unstable-1.5.4.0.tar.gz'
+    # version 'unstable-1.5.4.0'
+    # sha1 '69eb24999b70c46e0befd5eff4a9c9a70d218f41'
   end
 
   depends_on :automake
